4dfc2de19e310d7b9137e6715b04faf2d775e124
TRANHOANGTHUONG <tht1267@gmail.com>
2025-08-11 18:43:24 +0700

    test

--- rating_feedback_service/rating_feedback/wsgi.py
+++ rating_feedback_service/rating_feedback/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rating_feedback.settings')
 
 application = get_wsgi_application()
+                             
