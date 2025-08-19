719e6f0d37fe538abf00871bb8b36cf1229b619f
Nguyễn Công Phú <phunc5226@ut.edu.vn>
2025-08-19 18:08:04 +0700

    Update admin moderator service

--- admin-moderator-service/app/main.py
+++ admin-moderator-service/app/main.py
@@ -3,15 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .routers import services, violations, dashboard, users, tutors
 
-
-
 app = FastAPI(title="Admin & Moderation Service")
 Base.metadata.create_all(bind=engine)
 
-# Bật CORS cho phép frontend truy cập
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Có thể thay bằng domain frontend nếu muốn bảo mật hơn
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
