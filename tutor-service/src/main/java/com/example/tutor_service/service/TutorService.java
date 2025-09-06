e72d7c005c1e73e3f2e948d98c48dd2571b950f0
NguyenCong2011 <cong201120001@gmail.com>
2025-09-06 17:41:21 +0700

    search tutor

--- tutor-service/src/main/java/com/example/tutor_service/service/TutorService.java
+++ tutor-service/src/main/java/com/example/tutor_service/service/TutorService.java
@@ -1,17 +1,36 @@
 package com.example.tutor_service.service;
 
+import com.example.tutor_service.dto.response.SearchTutorResponse;
 import com.example.tutor_service.entity.Tutor;
 import com.example.tutor_service.repository.TutorRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TutorService {
     private final TutorRepository tutorRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public Tutor getTutorByUserId(Long userId) {
         return tutorRepository.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("Tutor not found"));
+    }
+
+    public void searchAndSendTutors(String requestId, String keyword, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Tutor> tutorPage = tutorRepository.findBySkillsContaining(keyword, pageable);
+
+        SearchTutorResponse response = new SearchTutorResponse();
+        response.setRequestId(requestId);
+        response.setTutors(tutorPage.getContent());
+        response.setTotalElements(tutorPage.getTotalElements());
+
+        kafkaTemplate.send("search-user-for-tutors", response);
+        System.out.println("Đã gửi response sang user-service để enrich username: " + response);
     }
 }
